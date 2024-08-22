d4b1014ca2a0cc691e7cedc6305de91fe771fbed
Cristian PC <cristianpc3087@outlook.com>
2024-08-21 20:03:37 -0500

    [FEAT] BDD con sus preguntas

--- examen/models.py
+++ examen/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Pregunta(models.Model):
+    enunciado = models.TextField()
+
+class Respuesta(models.Model):
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE)
+    texto = models.TextField()
+    es_correcta = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.pregunta.id}, {self.texto}, {self.es_correcta}" 
+
